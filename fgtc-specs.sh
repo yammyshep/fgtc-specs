e70f8c7784ac99230075be54e6f72ea0c4920048
YammyShep <jdbuelow@gmail.com>
2026-02-19 13:52:27 -0600

    Move things to functions

--- fgtc-specs.sh
+++ fgtc-specs.sh
@@ -2,94 +2,113 @@
 
 SERVER_URL=http://127.0.0.1:8000
 
+function get_processors_json {
+    PROCESSOR_MODELS=($(sudo dmidecode --string processor-version))
+
+    PROCESSORS_JSON="[]"
+    for i in "${!PROCESSOR_MODELS[@]}"; do
+        PROCESSORS_JSON=$(echo $PROCESSORS_JSON | jq \
+            --arg model "${PROCESSOR_MODELS[$i]}" \
+            '. += [{model: $model}]')
+    done
+    echo "$PROCESSORS_JSON"
+}
+
+function get_memory_json {
+    MEMORY_TYPES=($(sudo dmidecode --type memory | grep -E '^\s*Type:' | awk '{print $2}'))
+    MEMORY_SIZES=($(sudo dmidecode --type memory | grep -E '^\s*Size:' | awk '{print $2}'))
+    MEMORY_UNITS=($(sudo dmidecode --type memory | grep -E '^\s*Size:' | awk '{print $3}'))
+    MEMORY_SPEEDS=($(sudo dmidecode --type memory | grep -E '^\s*Speed:' | awk '{print $2}'))
+
+    MEMORY_JSON="[]"
+    for i in "${!MEMORY_TYPES[@]}"; do
+        MEMORY_JSON=$(echo $MEMORY_JSON | jq \
+            --arg type "${MEMORY_TYPES[$i]#DDR}" \
+            --arg size "$((${MEMORY_SIZES[$i]} * 1000))" \
+            --arg clock "${MEMORY_SPEEDS[$i]}" \
+            '. += [{type: $type, size: $size, clock: $clock}]')
+    done
+    echo "$MEMORY_JSON"
+}
+
+function get_lan_speed {
+    FASTEST_LINKSPEED=null
+    for i in $(netstat -i | cut -f1 -d" " | tail -n+3); do
+        LINKSPEED=$(ethtool --json $i 2> /dev/null | jq '.[0]."supported-link-modes"[-1]' | sed 's/[^0-9]*//g')
+        if (( LINKSPEED > FASTEST_LINKSPEED )); then
+            FASTEST_LINKSPEED=$LINKSPEED
+        fi
+    done
+    echo $FASTEST_LINKSPEED
+}
+
+function get_wlan_standard {
+    WIRELESS_INFO=$(iw list)
+    #TODO: Detect WiFi 7 (802.11be) adapters
+    #if [[ $(grep "" <<< $WIRELESS_INFO) ]]; then
+    #    echo "be"
+    #fi
+    if [[ $(grep "+HTC HE Supported" <<< $WIRELESS_INFO) ]]; then
+        echo "ax"
+    elif [[ $(grep "VHT Capabilities" <<< $WIRELESS_INFO) ]]; then
+        echo "ac"
+    elif [[ $(grep "HT Capability" <<< $WIRELESS_INFO) ]]; then
+        echo "n"
+    else
+        echo "none"
+    fi
+}
+
+function get_bluetooth {
+    if [[ -n "$(rfkill list | grep -i bluetooth)" ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+function get_batteries_json {
+    BATTERIES_JSON="[]"
+    for i in $(upower -e | grep battery_BAT); do
+        BATTERY_DESIGN=$(bc <<< "$(upower -i $i | grep energy-full-design: | sed 's/[^0-9\.]*//g')*1000")
+        BATTERY_REMAIN=$(bc <<< "$(upower -i $i | grep energy-full: | sed 's/[^0-9\.]*//g')*1000")
+        BATTERIES_JSON=$(echo $BATTERIES_JSON | jq \
+        --arg design "$(printf "%.0f" $BATTERY_DESIGN)" \
+        --arg remain "$(printf "%.0f" $BATTERY_REMAIN)" \
+        '. + [{design_capacity: $design, remaining_capacity: $remain}]')
+    done
+    echo "$BATTERIES_JSON"
+}
+
 TYPE=other
 MANUFACTURER=($(sudo dmidecode --string system-manufacturer))
 MODEL=$(sudo dmidecode --string system-product-name)
 OPERATING_SYSTEM=$(cat /etc/*-release | awk '/PRETTY_NAME/' | cut -d\" -f2)
-
-PROCESSOR_MODELS=$(sudo dmidecode --string processor-version)
-
-MEMORY_TYPES=($(sudo dmidecode --type memory | grep -E '^\s*Type:' | awk '{print $2}'))
-MEMORY_SIZES=($(sudo dmidecode --type memory | grep -E '^\s*Size:' | awk '{print $2}'))
-MEMORY_UNITS=($(sudo dmidecode --type memory | grep -E '^\s*Size:' | awk '{print $3}'))
-MEMORY_SPEEDS=($(sudo dmidecode --type memory | grep -E '^\s*Speed:' | awk '{print $2}'))
-
-MEMORY_JSON="[]"
-for i in "${!MEMORY_TYPES[@]}"; do
-    MEMORY_JSON=$(echo $MEMORY_JSON | jq \
-        --arg type "${MEMORY_TYPES[$i]#DDR}" \
-        --arg size "$((${MEMORY_SIZES[$i]} * 1000))" \
-        --arg clock "${MEMORY_SPEEDS[$i]}" \
-        '. += [{type: $type, size: $size, clock: $clock}]')
-done
-
-PROCESSORS_JSON="[]"
-for i in "${!PROCESSOR_MODELS[@]}"; do
-    PROCESSORS_JSON=$(echo $PROCESSORS_JSON | jq \
-        --arg model "${PROCESSOR_MODELS[$i]}" \
-        '. += [{model: $model}]')
-done
-
-FASTEST_LINKSPEED=null
-for i in $(netstat -i | cut -f1 -d" " | tail -n+3); do
-    LINKSPEED=$(ethtool --json $i 2> /dev/null | jq '.[0]."supported-link-modes"[-1]' | sed 's/[^0-9]*//g')
-    if (( LINKSPEED > FASTEST_LINKSPEED )); then
-        FASTEST_LINKSPEED=$LINKSPEED
-    fi
-done
-
-WIRELESS="none"
-WIRELESS_INFO=$(iw list)
-if [[ $(grep "HT Capability" <<< $WIRELESS_INFO) ]]; then
-    WIRELESS="n"
-fi
-if [[ $(grep "VHT Capabilities" <<< $WIRELESS_INFO) ]]; then
-    WIRELESS="ac"
-fi
-if [[ $(grep "+HTC HE Supported" <<< $WIRELESS_INFO) ]]; then
-    WIRELESS="ax"
-fi
-#TODO: Detect WiFi 7 (802.11be) adapters
-#if [[ $(grep "" <<< $WIRELESS_INFO) ]]; then
-#    WIRELESS="be"
-#fi
-
-BLUETOOTH=false
-if [[ -n "$(rfkill list | grep -i bluetooth)" ]]; then
-    BLUETOOTH=true
-fi
-
-BATTERIES_JSON="[]"
-for i in $(upower -e | grep battery_BAT); do
-    BATTERY_DESIGN=$(bc <<< "$(upower -i $i | grep energy-full-design: | sed 's/[^0-9\.]*//g')*1000")
-    BATTERY_REMAIN=$(bc <<< "$(upower -i $i | grep energy-full: | sed 's/[^0-9\.]*//g')*1000")
-    BATTERIES_JSON=$(echo $BATTERIES_JSON | jq \
-    --arg design "$(printf "%.0f" $BATTERY_DESIGN)" \
-    --arg remain "$(printf "%.0f" $BATTERY_REMAIN)" \
-    '. + [{design_capacity: $design, remaining_capacity: $remain}]')
-done
 
 BUILD_JSON=$( jq -n \
     --arg type "$TYPE" \
     --arg manufacturer "$MANUFACTURER" \
     --arg model "$MODEL" \
     --arg os "$OPERATING_SYSTEM" \
-    --arg wired "$FASTEST_LINKSPEED" \
-    --arg wireless null \
-    --arg bluetooth "$BLUETOOTH" \
-    --argjson processors "$PROCESSORS_JSON" \
-    --argjson memory "$MEMORY_JSON" \
-    --argjson batteries "$BATTERIES_JSON" \
+    --arg wired "$(get_lan_speed)" \
+    --arg wireless "$(get_wlan_standard)" \
+    --arg bluetooth "$(get_bluetooth)" \
+    --argjson processors "$(get_processors_json)" \
+    --argjson memory "$(get_memory_json)" \
+    --argjson batteries "$(get_batteries_json)" \
     '{type: $type, manufacturer: $manufacturer, model: $model, operating_system: $os, wired_networking: $wired, wireless_networking: $wireless, bluetooth: $bluetooth, processors: $processors, memory: $memory, batteries: $batteries}'
 )
 
 echo $BUILD_JSON
-echo $WIRELESS
 
-exit 0
-RESPONSE=$(curl --json "$BUILD_JSON" ${SERVER_URL}/build)
+if [[ " $* " != *" --no-submit "* ]]; then
+    RESPONSE=$(curl --json "$BUILD_JSON" ${SERVER_URL}/build 2> /dev/null)
 
-ID=$(echo $RESPONSE | jq '.id' | tr -d '"')
-echo $ID
+    ID=$(echo $RESPONSE | jq '.id' | tr -d '"')
+    echo "Build ID: $ID"
+    
+    if [[ " $* " != *" --no-open "* ]]; then
+        xdg-open ${SERVER_URL}/build/create?edit=${ID}
+    fi
+fi
 
-xdg-open ${SERVER_URL}/build/create?edit=${ID}
